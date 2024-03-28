ed739397003edccd4fa5ed7f2ed7e7646cc2bf98
jasminisantos <jasminirebeca10@gmail.com>
2024-03-28 20:54:22 -0300

    Initial Commit

---
+++ Ink of The Ancient Stones/Ink_of_The_Ancient_StonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ink_of_The_Ancient_StonesApp.swift
+//  Ink of The Ancient Stones
+//
+//  Created by Jasmini Rebecca Gomes dos Santos on 28/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Ink_of_The_Ancient_StonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
